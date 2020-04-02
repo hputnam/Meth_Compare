b3fbaadac9782fd1484fac1031f19bcdd641bde5
Hollie Putnam <hollieputnam@gmail.com>
2020-04-02 10:16:58 -0400

    updating circos plots for Mcap, Pact, and C1

--- scripts/Circos_Meth_Plot.R
+++ scripts/Circos_Meth_Plot.R
@@ -168,84 +168,86 @@ circos.genomicTrack(Mcap_union.RRBS, stack=FALSE, ylim=c(0,1),track.height = 0.1
 dev.off()
 
 
-# ##### Sym Cladicopium C1 #####
-# #load in all genes gff from a genome
-# C1.cytoband <- read.table("data/SymbC1.Gene_Models.GFF.GFF3", sep = "\t",  header = FALSE)
-# 
-# C1.cytoband <- C1.cytoband %>%
-#   filter(V3 == 'gene')
-# 
-# #remove extra coloumns from gene gff
-# C1.cytoband.df <- C1.cytoband[,c(1,4,5,9)]
-# str(C1.cytoband.df)
-# head(C1.cytoband.df)
-# 
-# #read in the 5x coverage bedgraph of CpG for all 18 samples, 3 reps x 3 methods x 2 species
-# C1 <- read.table("data/Pact-C1_union_5x.bedgraph", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
-# head(C1)
-# str(C1)
-# 
-# #examine scaffolds for the number of genes in a scaffold 
-# sizes <- C1.cytoband.df %>% 
-#   group_by(V1) %>% 
-#   summarise(count=n()) %>%
-#   arrange(-count)
-# 
-# which(C1.cytoband.df$V1=='SymbC1.scaffold1')
-# 
-# C1.cytoband.df <- C1.cytoband.df %>%
-#   filter(V1 == "SymbC1.scaffold1") #| V1 == "SymbC1.scaffold2" | V1 == "SymbC1.scaffold4"| V1 == "SymbC1.scaffold5" | V1 == "SymbC1.scaffold7")
-#         # | V1 == "SymbC1.scaffold9" | V1 == "SymbC1.scaffold11" | V1 == "SymbC1.scaffold16"| V1 == "SymbC1.scaffold19" | V1 == "SymbC1.scaffold20" )
-# 
-# C1_union <- C1 %>%
-#   filter(chrom== "SymbC1.scaffold1")#| chrom== "SymbC1.scaffold2" | chrom== "SymbC1.scaffold4"| chrom== "SymbC1.scaffold5" | chrom== "SymbC1.scaffold7")
-#          #| chrom== "SymbC1.scaffold9" | chrom== "SymbC1.scaffold11" | chrom== "SymbC1.scaffold16"| chrom== "SymbC1.scaffold19" | chrom== "SymbC1.scaffold20" )
-# 
-# which(C1_union$chrom=='SymbC1.scaffold1')
-# 
-# #set all CpG data as numeric
-# C1_union$X1 <-as.numeric(C1_union$X1)
-# C1_union$X2 <-as.numeric(C1_union$X2)
-# C1_union$X3 <-as.numeric(C1_union$X3)
-# C1_union$X4 <-as.numeric(C1_union$X4)
-# C1_union$X5 <-as.numeric(C1_union$X5)
-# C1_union$X6 <-as.numeric(C1_union$X6)
-# C1_union$X7 <-as.numeric(C1_union$X7)
-# C1_union$X8 <-as.numeric(C1_union$X8)
-# C1_union$X9 <-as.numeric(C1_union$X9)
-# head(C1_union)
-# 
-# #scale the data from 0-1
-# C1_union.WGBS <- C1_union %>% mutate(avg = rowMeans(.[4:6], na.rm=TRUE))
-# C1_union.WGBS <- C1_union.WGBS[,c(1,2,3,13)]
-# C1_union.WGBS$avg <- C1_union.WGBS$avg/100
-# C1_union.RRBS <- C1_union %>% mutate(avg = rowMeans(.[7:9], na.rm=TRUE))
-# C1_union.RRBS <- C1_union.RRBS[,c(1,2,3,13)]
-# C1_union.RRBS$avg <- C1_union.RRBS$avg/100
-# C1_union.MBDBS <- C1_union %>% mutate(avg = rowMeans(.[10:12], na.rm=TRUE))
-# C1_union.MBDBS <- C1_union.MBDBS[,c(1,2,3,13)]
-# C1_union.MBDBS$avg <- C1_union.MBDBS$avg/100
-# 
-# #plot CpG of top 10 scaffolds with the highest numbers of genes
-# #outer track = WGBS in blue
-# #middle track = MBDBS in cyan
-# #inner track = RRBS in green
-# pdf('Output/C1_genes_CpG_3methods_top1.pdf')
-# circos.clear()
-# 
-# C1.cytoband.df <- C1.cytoband.df[20,]
-# 
-# circos.initializeWithIdeogram(C1.cytoband.df, species = NULL, sort.chr = TRUE)
-# # circos.genomicTrack(C1_union.WGBS, stack=FALSE, ylim=c(0,1), track.height = 0.1,
-# #                     panel.fun = function(region, value, ...) {
-# #                       circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="blue")
-# #                     })
-# # circos.genomicTrack(C1_union.MBDBS, stack=FALSE, ylim=c(0,1), track.height = 0.1,
-# #                     panel.fun = function(region, value, ...) {
-# #                       circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="cyan")
-# #                     })
-# circos.genomicTrack(C1_union.RRBS, stack=FALSE, ylim=c(0,1),track.height = 0.1,
-#                     panel.fun = function(region, value, ...) {
-#                       circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="green")
-#                     })
-# dev.off()
+##### Sym Cladicopium C1 #####
+#load in all genes gff from a genome
+C1.cytoband <- read.table("data/SymbC1.Gene_Models.GFF.GFF3", sep = "\t",  header = FALSE)
+
+C1.cytoband <- C1.cytoband %>%
+  filter(V3 == 'gene')
+
+#remove extra coloumns from gene gff
+C1.cytoband.df <- C1.cytoband[,c(1,4,5,6,9)]
+str(C1.cytoband.df)
+head(C1.cytoband.df)
+C1.cytoband.df$V1 <- as.character(C1.cytoband.df$V1)
+str(C1.cytoband.df)
+  
+#read in the 5x coverage bedgraph of CpG for all 18 samples, 3 reps x 3 methods x 2 species
+C1 <- read.table("data/Pact-C1_union_5x.bedgraph", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+head(C1)
+str(C1)
+
+#examine scaffolds for the number of genes in a scaffold 
+sizes <- C1.cytoband.df %>% 
+  group_by(V1) %>% 
+  summarise(count=n()) %>%
+  arrange(-count)
+
+which(C1.cytoband.df$V1=='SymbC1.scaffold1')
+
+C1.cytoband.df <- C1.cytoband.df %>%
+  filter(V1 == "SymbC1.scaffold1" | V1 == "SymbC1.scaffold2" | V1 == "SymbC1.scaffold4"| V1 == "SymbC1.scaffold5" | V1 == "SymbC1.scaffold7" |
+   V1 == "SymbC1.scaffold9" | V1 == "SymbC1.scaffold11" | V1 == "SymbC1.scaffold16"| V1 == "SymbC1.scaffold19" | V1 == "SymbC1.scaffold20")
+
+C1_union <- C1 %>%
+  filter(chrom== "SymbC1.scaffold1"| chrom== "SymbC1.scaffold2" | chrom== "SymbC1.scaffold4"| chrom== "SymbC1.scaffold5" | chrom== "SymbC1.scaffold7"
+       | chrom== "SymbC1.scaffold9" | chrom== "SymbC1.scaffold11" | chrom== "SymbC1.scaffold16"| chrom== "SymbC1.scaffold19" | chrom== "SymbC1.scaffold20" )
+
+which(C1_union$chrom=='SymbC1.scaffold1')
+
+#set all CpG data as numeric
+C1_union$X1 <-as.numeric(C1_union$X1)
+C1_union$X2 <-as.numeric(C1_union$X2)
+C1_union$X3 <-as.numeric(C1_union$X3)
+C1_union$X4 <-as.numeric(C1_union$X4)
+C1_union$X5 <-as.numeric(C1_union$X5)
+C1_union$X6 <-as.numeric(C1_union$X6)
+C1_union$X7 <-as.numeric(C1_union$X7)
+C1_union$X8 <-as.numeric(C1_union$X8)
+C1_union$X9 <-as.numeric(C1_union$X9)
+head(C1_union)
+
+#scale the data from 0-1
+C1_union.WGBS <- C1_union %>% mutate(avg = rowMeans(.[4:6], na.rm=TRUE))
+C1_union.WGBS <- C1_union.WGBS[,c(1,2,3,13)]
+C1_union.WGBS$avg <- C1_union.WGBS$avg/100
+C1_union.RRBS <- C1_union %>% mutate(avg = rowMeans(.[7:9], na.rm=TRUE))
+C1_union.RRBS <- C1_union.RRBS[,c(1,2,3,13)]
+C1_union.RRBS$avg <- C1_union.RRBS$avg/100
+C1_union.MBDBS <- C1_union %>% mutate(avg = rowMeans(.[10:12], na.rm=TRUE))
+C1_union.MBDBS <- C1_union.MBDBS[,c(1,2,3,13)]
+C1_union.MBDBS$avg <- C1_union.MBDBS$avg/100
+
+#plot CpG of top 10 scaffolds with the highest numbers of genes
+#outer track = WGBS in blue
+#middle track = MBDBS in cyan
+#inner track = RRBS in green
+pdf('Output/C1_genes_CpG_3methods_top1.pdf')
+circos.clear()
+
+#C1.cytoband.df <- C1.cytoband.df[20,]
+
+circos.initializeWithIdeogram(C1.cytoband.df, species = NULL, sort.chr = TRUE)
+circos.genomicTrack(C1_union.WGBS, stack=FALSE, ylim=c(0,1), track.height = 0.1,
+                    panel.fun = function(region, value, ...) {
+                      circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="blue")
+                    })
+circos.genomicTrack(C1_union.MBDBS, stack=FALSE, ylim=c(0,1), track.height = 0.1,
+                    panel.fun = function(region, value, ...) {
+                      circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="cyan")
+                    })
+circos.genomicTrack(C1_union.RRBS, stack=FALSE, ylim=c(0,1),track.height = 0.1,
+                    panel.fun = function(region, value, ...) {
+                      circos.genomicPoints(region, value, pch = 16, cex = 0.2, col="green")
+                    })
+dev.off()
